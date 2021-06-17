c1c46733a4e82417029d10f1276be63df1f84e85
Juan Bravo <jnnaudiovideo@gmail.com>
2021-06-17 00:04:54 -0400

    prueba git

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  componentes: Componente[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: "/action-sheet"
+    },
+    {
+      icon: 'alert-circle-outline',
+      name: 'Alert',
+      redirectTo: "/alert"
+    },
+    {
+      icon: 'beaker-outline',
+      name: 'Avatar',
+      redirectTo: "/avatar"
+    },
+    {
+      icon: 'radio-button-off-outline',
+      name: 'Buttons',
+      redirectTo: "/button"
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
